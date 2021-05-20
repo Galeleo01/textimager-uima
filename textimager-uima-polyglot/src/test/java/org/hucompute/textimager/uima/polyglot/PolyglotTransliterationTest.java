8ce03b1472ed61d0dcfd7fe9f824a856fdbd05ff
Galeleo01 <gsiwiecki@gmail.com>
2021-05-20 14:35:18 -0400

    Polyglot test erfolgreich

--- textimager-uima-polyglot/src/test/java/org/hucompute/textimager/uima/polyglot/PolyglotTransliterationTest.java
+++ textimager-uima-polyglot/src/test/java/org/hucompute/textimager/uima/polyglot/PolyglotTransliterationTest.java
@@ -4,6 +4,7 @@ import static org.apache.uima.fit.util.JCasUtil.select;
 import static org.junit.Assert.*;
 import static org.apache.uima.fit.factory.AnalysisEngineFactory.createEngineDescription;
 
+import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Sentence;
 import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token;
 import org.apache.uima.analysis_engine.AnalysisEngineDescription;
 import org.apache.uima.fit.factory.JCasFactory;
@@ -13,6 +14,9 @@ import org.apache.uima.jcas.JCas;
 import org.junit.Test;
 
 import tansliterationAnnotation.type.TransliterationAnnotation;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
 * PolyglotTransliterationTest
@@ -55,29 +59,37 @@ public class PolyglotTransliterationTest {
 
 		SimplePipeline.runPipeline(cas, polyglottransliteration);
 		// Sample Text
-		String outputCorrectToken = "Das | Essen | gestern | Nacht | hat | großartig | geschmeckt | , | obwohl | der | Fisch | schlecht | . |";
-		String outputCorrectValue = "داس | يسين | جتيرن | ناكهت | هات |  | جكهميكت |  | وبووهل | دير | فيش | شليكهت | ";
-		String outputCorrectBegin = "0 | 4 | 10 | 18 | 24 | 28 | 38 | 48 | 50 | 57 | 61 | 67 | ";
-		String outputCorrectEnd = "3 | 9 | 17 | 23 | 27 | 37 | 48 | 49 | 56 | 60 | 66 | 75 | ";
+		String outputCorrectToken = "Das | Essen | gestern | Nacht | hat | großartig | geschmeckt | obwohl | der | Fisch | schlecht | ";
+		String outputCorrectValue ="داس | يسين | جتيرن | ناكهت | هات |  | جكهميكت | وبووهل | دير | فيش | شليكهت";
+		String outputCorrectBegin = "0 | 4 | 10 | 18 | 24 | 28 | 38 | 50 | 57 | 61 | 67 | ";
+		String outputCorrectEnd = "3 | 9 | 17 | 23 | 27 | 37 | 48 | 56 | 60 | 66 | 75 | ";
 		
 		// Generated text with library
 		String outputTestToken = "";
 		String outputTestValue = "";
 		String outputTestBegin = "";
 		String outputTestEnd = "";
-		
+
+		List<Integer> sen = new ArrayList<>();
+		for (Sentence sentence : JCasUtil.select(cas, Sentence.class))
+		{
+			sen.add(sentence.getEnd());
+
+		}
 		// Loop over different TransliterationAnnotation-Tags and create the UIMA-Output.
 		for (TransliterationAnnotation t : JCasUtil.select(cas, TransliterationAnnotation.class)) {
 			outputTestToken = outputTestToken + t.getCoveredText() + " | ";
-			outputTestValue = outputTestValue + t.getValue() + " | ";
+			if(sen.contains(t.getEnd()+1))
+			{outputTestValue = outputTestValue + t.getValue();}
+			else{outputTestValue = outputTestValue + t.getValue() + " | ";}
 			outputTestBegin = outputTestBegin + t.getBegin() + " | ";
 			outputTestEnd = outputTestEnd + t.getEnd() + " | ";
         }
 		
 		// JUnit-Test: CoveredText, Value, Begin, End
 		assertEquals(outputCorrectToken, outputTestToken);
-		assertEquals(outputCorrectValue, outputTestValue);
 		assertEquals(outputCorrectBegin, outputTestBegin);
 		assertEquals(outputCorrectEnd, outputTestEnd);
+		assertEquals(outputCorrectValue, outputTestValue);
 	}
 }
