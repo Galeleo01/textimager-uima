055c440d88e0d635d1b2dd421c701d4f19124c25
Galeleo01 <gsiwiecki@gmail.com>
2021-05-21 11:47:42 -0400

    JulieLab Dependecies

---
+++ JuliLab/src/test/java/jPOSTest.java
@@ -0,0 +1,22 @@
+import static org.junit.Assert.*;
+import static org.apache.uima.fit.factory.AnalysisEngineFactory.createEngineDescription;
+
+import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Sentence;
+import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token;
+import org.apache.uima.analysis_engine.AnalysisEngineDescription;
+import org.apache.uima.fit.factory.JCasFactory;
+import org.apache.uima.fit.pipeline.SimplePipeline;
+import org.apache.uima.fit.util.JCasUtil;
+import org.apache.uima.jcas.JCas;
+import org.junit.Test;
+
+
+public class jPOSTest {
+
+    @Test
+    public void  firstTest() throws Exception
+    {
+
+
+    }
+}
