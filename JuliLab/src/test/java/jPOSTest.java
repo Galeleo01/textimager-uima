49576b31a070e70cb9076482d4a98ab544f64bcd
jackykang061233 <43503345+jackykang061233@users.noreply.github.com>
2021-05-24 20:18:32 +0200

    JPOSTest
    Habe JPOSTest fertig gemacht. Wir brauchen aber java 11

--- JuliLab/src/test/java/jPOSTest.java
+++ JuliLab/src/test/java/jPOSTest.java
@@ -1,22 +1,150 @@
-import static org.junit.Assert.*;
-import static org.apache.uima.fit.factory.AnalysisEngineFactory.createEngineDescription;
+//
+//import static org.junit.Assert.*;
+//import static org.apache.uima.fit.factory.AnalysisEngineFactory.createEngineDescription;
+//
+//
+////import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token;
+//import de.julielab.jcore.types.Sentence;
+//import org.apache.uima.UIMAException;
+//import org.apache.uima.UIMAFramework;
+//import org.apache.uima.analysis_engine.AnalysisEngine;
+//import org.apache.uima.analysis_engine.AnalysisEngineDescription;
+//import org.apache.uima.fit.factory.JCasFactory;
+//import org.apache.uima.fit.pipeline.SimplePipeline;
+//import org.apache.uima.fit.util.JCasUtil;
+//import org.apache.uima.jcas.JCas;
+//import org.apache.uima.resource.ResourceSpecifier;
+//import org.apache.uima.util.XMLInputSource;
+//import org.junit.Test;
+//import de.julielab.jcore.ae.jpos.postagger.POSAnnotator;
+//import de.julielab.jcore.types.Token;
+//
+//
+//
+//public class jPOSTest {
+//    String text = "Der kleine Baum";
+//    String postags = "ART;ADJA;NN;";
+//    public JCas initCas() throws UIMAException {
+//        final JCas jcas = JCasFactory.createJCas();
+//        jcas.reset();
+//        jcas.setDocumentText(text);
+//        final Sentence s1 = new Sentence(jcas);
+//        s1.setBegin(0);
+//        s1.setEnd(text.length());
+//        s1.addToIndexes();
+//
+//        final Token t1 = new Token(jcas);
+//
+//        t1.setBegin(0);
+//        t1.setEnd(3);
+//        t1.addToIndexes();
+//
+//
+//        final Token t2 = new Token(jcas);
+//        t2.setBegin(4);
+//        t2.setEnd(10);
+//        t2.addToIndexes();
+//
+//        final Token t3 = new Token(jcas);
+//        t3.setBegin(11);
+//        t3.setEnd(15);
+//        t3.addToIndexes();
+//        return jcas;
+//    }
+//
+//    @Test
+//    public void  firstTest() throws Exception {
+//
+//        AnalysisEngineDescription posAnnotator = createEngineDescription(POSAnnotator.class);
+//        final JCas jcas = initCas();
+//        // get test cas with sentence annotation
+//        ;
+//        SimplePipeline.runPipeline(jcas, posAnnotator);
+//
+//    }
+//}
+/**
+ * Copyright (c) 2015, JULIE Lab.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the BSD-2-Clause License
+ */
 
-import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Sentence;
-import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token;
-import org.apache.uima.analysis_engine.AnalysisEngineDescription;
+
+import de.julielab.jcore.ae.jpos.postagger.POSAnnotator;
+import de.julielab.jcore.types.STTSMedPOSTag;
+import de.julielab.jcore.types.Sentence;
+import de.julielab.jcore.types.Token;
+import org.apache.uima.UIMAFramework;
+import org.apache.uima.analysis_engine.AnalysisEngine;
+import org.apache.uima.cas.FSIterator;
+import org.apache.uima.fit.factory.AggregateBuilder;
 import org.apache.uima.fit.factory.JCasFactory;
 import org.apache.uima.fit.pipeline.SimplePipeline;
 import org.apache.uima.fit.util.JCasUtil;
 import org.apache.uima.jcas.JCas;
+import org.apache.uima.jcas.JFSIndexRepository;
+import org.apache.uima.jcas.cas.FSArray;
+import org.apache.uima.jcas.tcas.Annotation;
+import org.apache.uima.resource.ResourceSpecifier;
+import org.apache.uima.util.XMLInputSource;
 import org.junit.Test;
+import java.util.*;
 
+import static org.apache.uima.fit.factory.AnalysisEngineFactory.createEngineDescription;
+import static org.junit.Assert.assertEquals;
 
 public class jPOSTest {
 
-    @Test
-    public void  firstTest() throws Exception
-    {
+    String[] Text = {"Der kleine Baum", "Am blauen Himmel ziehen die Wolken"};
+    String[] Postags = {"ART;ADJA;NN;", "APPRART;ADJA;NN;VVFIN;ART;NN"};
 
+    public void initCas(final JCas jcas, String text) {
+        jcas.reset();
+        jcas.setDocumentText(text);
+        final Sentence sentence = new Sentence(jcas);
+        sentence.setBegin(0);
+        sentence.setEnd(text.length());
+        sentence.addToIndexes();
+
+        String[] words = text.split(" ");
+
+        //initialize index
+        int index_start = 0;
+        int index_end = 0;
+
+        for (String word : words) {
+            final Token token = new Token(jcas);
+            index_end = index_start + word.length();
+            token.setBegin(index_start);
+            token.setEnd(index_end);
+            token.addToIndexes();
+            index_start = index_end + 1;
+        }
+
+    }
+
+    @Test
+    public void firstTest() throws Exception {
+
+        XMLInputSource posXML = new XMLInputSource("src/test/resources/POSTagAnnotatorTest.xml");
+        ResourceSpecifier posSpec = UIMAFramework.getXMLParser().parseResourceSpecifier(posXML);
+        AnalysisEngine posAnnotator = UIMAFramework.produceAnalysisEngine(posSpec);
+
+        final JCas jcas = JCasFactory.createJCas();
+        // get test cas with sentence annotation
+        for (int i=0; i<Text.length; i++){
+            initCas(jcas, Text[i]);
+
+            posAnnotator.process(jcas);
+            // get the offsets of the sentences
+            String predictedpostag = "";
+            for (Token token : JCasUtil.select(jcas, Token.class))
+            {
+                predictedpostag = predictedpostag + token.getPosTag(0).getValue() + ";";
+            }
+            String correctpostag = Postags[i];
+            assertEquals(correctpostag, predictedpostag);
+        }
 
     }
 }
