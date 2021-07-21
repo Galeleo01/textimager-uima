2afc7f76bf1b424fa795dd345ec7d1b94f7d5421
jackykang061233 <43503345+jackykang061233@users.noreply.github.com>
2021-07-21 12:00:24 +0200

    first

---
+++ textimager-uima-juilie/src/test/java/EccodeTest.java
@@ -0,0 +1,8 @@
+import org.apache.uima.analysis_engine.AnalysisEngine;
+import org.apache.uima.fit.factory.JCasFactory;
+import org.apache.uima.jcas.JCas;
+import org.junit.Test;
+
+public class EccodeTest {
+
+}
